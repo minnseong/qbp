3f8293d62766daaba3b91a9b2523577e294dc311
minseong <kdsvip5@naver.com>
2022-08-18 13:28:48 +0900

    initial commit & mysql connect

---
+++ src/main/java/com/min/qbp/QbpApplication.java
@@ -0,0 +1,13 @@
+package com.min.qbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QbpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/min/qbp/QbpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.min.qbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
